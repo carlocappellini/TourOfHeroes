54fc2f8123228964e4f7633da0d4fcc56aa73eea
Bolimbo <carlo.cappellini@yahoo.com>
2018-07-19 23:06:08 -0500

    Annotated hero property with @Input decorator, external hero component binds to hero detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
+import {HeroDetailComponent} from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
