3cb86a2d0e4bb782249622e9f1a8474e1e1282e0
Bolimbo <carlo.cappellini@yahoo.com>
2018-07-22 21:27:34 -0500

    ng generated component messages

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 import {Observable, of} from 'rxjs';
+import { MessageService } from './message.service';
 
 
 @Injectable({
@@ -10,7 +11,8 @@ import {Observable, of} from 'rxjs';
 
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes')
     return of (HEROES);
   }
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
