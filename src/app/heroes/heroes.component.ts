52cfc159396cb463686479fd4006b10ce5b13749
Bolimbo <carlo.cappellini@yahoo.com>
2018-07-24 22:58:44 -0500

    deleted hero detail from heroes component template

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
 import {HeroService} from '../hero.service';
-import {HeroDetailComponent} from '../hero-detail/hero-detail.component';
+
 
 @Component({
   selector: 'app-heroes',
