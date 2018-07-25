fe93c9825a964f4b2ed05dbf6da6cb155d07c706
Bolimbo <carlo.cappellini@yahoo.com>
2018-07-24 23:00:28 -0500

    imported and added path for hero detail component in app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   { path: 'heroes',
@@ -12,6 +13,10 @@ const routes: Routes = [
   { path: '',
     redirectTo: '/dashboard',
     pathMatch: 'full' },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  }
 ];
 
 @NgModule({
